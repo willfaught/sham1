5b7c27f81429731ad5b19d3a36b03ddcc6caeb81
Will Faught <wfaught@prospero.local>
2008-04-04 23:12:49 -0700

    Got fib working.

--- test/Simple.hs
+++ test/Simple.hs
@@ -2,16 +2,10 @@
 
 module Simple where {
 
-a = [1, 2, 3];
-
-b = [4, 5, 6];
-
 zipWith f x y = if null x then [] else (:) (f (head x) (head y)) (zipWith f (tail x) (tail y));
-
-c = zipWith (+) a b;
 
 x !! n = if (==) n 0 then head x else (!!) (tail x) ((-) n 1);
 
-d = (!!) c 2
+fib = (:) 0 ((:) 1 (zipWith (+) fib (tail fib)))
 
 }
