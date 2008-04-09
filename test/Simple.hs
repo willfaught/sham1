863bdc676120e23a5a93f19b880496016e07183a
Will Faught <wfaught@pcp042623pcs.wireless.calpoly.edu>
2008-04-08 18:12:12 -0700

    Fixed some bugs with decl-term in term-type in hs-tc.ss.  Updated the test cases to match new types for functions (now expects full functio type to be given, not just the param types).

--- test/Simple.hs
+++ test/Simple.hs
@@ -2,14 +2,16 @@
 
 module Simple where {
 
-zip x y = if null x then [] else (:) (head x, head y) (zip (tail x) (tail y));
+--zip x y = if null x then [] else (:) (head x, head y) (zip (tail x) (tail y));
 
-zipWith f x y = if null x then [] else (:) (f (head x) (head y)) (zipWith f (tail x) (tail y));
+--zipWith f x y = if null x then [] else (:) (f (head x) (head y)) (zipWith f (tail x) (tail y));
 
-x !! n = if (==) n 0 then head x else (!!) (tail x) ((-) n 1);
+--x !! n = if (==) n 0 then head x else (!!) (tail x) ((-) n 1);
 
-x ++ y = if null x then y else (:) (head x) ((++) (tail x) y);
+--x ++ y = if null x then y else (:) (head x) ((++) (tail x) y);
 
-fib = (:) 0 ((:) 1 (zipWith (+) fib (tail fib)))
+--fib = (:) 0 ((:) 1 (zipWith (+) fib (tail fib)))
+
+a = 1 :: a
 
 }
