76799ed3bc8a7b2457eaf06ef5e5140b9b70171c
Will Faught <wfaught@prospero.local>
2008-05-20 01:26:52 -0700

    Changed mlFunction2 to take a 2-tuple.  Changed mlTuple to be a (Bool, [Char]).

--- lib/MLTest.hs
+++ lib/MLTest.hs
@@ -7,11 +7,11 @@ import "test.ocaml";
 ml1 = :ml Bool "mlBoolean1";
 ml2 = :ml Bool "mlBoolean2";
 ml3 = :ml a -> a "mlFunction1";
-ml4 = :ml (a, (b, (c, (d, e)))) -> (e, (d, (c, (b, a)))) "mlFunction2";
+ml4 = :ml (a, b) -> (b, a) "mlFunction2";
 ml5 = :ml Int -> Int "mlFunction3";
-ml6 = :ml Int "mlInt";
+ml6 = :ml Int "mlInteger";
 ml7 = :ml [Char] "mlString";
-ml8 = :ml (Bool, (Bool, (a -> a, (Int, [Char])))) "mlTuple";
+ml8 = :ml (Bool, [Char]) "mlTuple";
 ml9 = :ml () "mlUnit"
 
 }
