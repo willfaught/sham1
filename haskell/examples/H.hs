cbb8dc5616ca4c558db10ccc0fd563bf86edc012
Will Faught <wfaught@prospero.local>
2008-10-25 18:28:18 -0700

    Added H.hs.

---
+++ haskell/examples/H.hs
@@ -0,0 +1,21 @@
+#reader (lib "Reader.ss" "sham" "haskell")
+
+module H {
+
+boolean1 = True;
+
+boolean2 = False;
+
+function1 x = x;
+
+function2 x y = x;
+
+integer = 1;
+
+string = "test";
+
+tuple = (1, 2);
+
+unit = ()
+
+}
