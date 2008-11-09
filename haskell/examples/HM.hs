9701b250d84111cbdccea0c5acacbaded0e8415b
Will Faught <wfaught@prospero.local>
2008-11-09 01:26:09 -0800

    HM.hs: Renamed the vars.

--- haskell/examples/HM.hs
+++ haskell/examples/HM.hs
@@ -1,17 +1,25 @@
 #reader (lib "Reader.ss" "sham" "haskell")
 
-module MLTest where {
+module HM where {
 
 import "ml.ml";
 
-ml1 = :ml Bool "mlBoolean1";
-ml2 = :ml Bool "mlBoolean2";
-ml3 = :ml a -> a "mlFunction1";
-ml4 = :ml (a, b) -> (b, a) "mlFunction2";
-ml5 = :ml Int -> Int "mlFunction3";
-ml6 = :ml Int "mlInteger";
-ml7 = :ml [Char] "mlString";
-ml8 = :ml (Bool, [Char]) "mlTuple";
-ml9 = :ml () "mlUnit"
+hmBoolean1 = :ml Bool "mlBoolean1";
+
+hmBoolean2 = :ml Bool "mlBoolean2";
+
+hmFunction1 = :ml a -> a "mlFunction1";
+
+hmFunction2 = :ml (a, b) -> (b, a) "mlFunction2";
+
+hmFunction3 = :ml Int -> Int "mlFunction3";
+
+hmInteger = :ml Int "mlInteger";
+
+hmString = :ml String "mlString";
+
+hmTuple = :ml (Bool, [Char]) "mlTuple";
+
+hmUnit = :ml () "mlUnit"
 
 }
